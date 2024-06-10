21293e0ff0b2232b6ee07c1ce5169844f1e9766e
yahya akli <akli.yahya05@gmail.com>
2024-06-10 20:48:06 +0100

    yahya

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -67,7 +67,7 @@ const ContactPage = () => {
           ref={form}
           className="h-1/2 lg:h-full lg:w-1/2 bg-red-50 rounded-xl text-xl flex flex-col gap-8 justify-center p-24"
         >
-          <span>Dear Lama Dev,</span>
+          <span>Dear DuCodex,</span>
           <textarea
             rows={6}
             className="bg-transparent border-2 p-3 border-black outline-none resize-none"
